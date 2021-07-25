affac8f1d8abb13b20f3e57ae85a5a9f28a068f3
Amelia Meyer <agmlego@gmail.com>
2021-07-25 15:24:24 -0400

    Calendar should clear before drawing.

--- cal_clock.py
+++ cal_clock.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     logger.info('Starting!')
 
     if 'epd12in48b' in sys.modules:
-        eink = clock.EPD_Clock(config, portrait=True)
+        eink = clock.EPD_Clock(config, portrait=True, clear=True)
     else:
         logger.warning(
             f'Not e-ink, will use PIL simulation instead')
